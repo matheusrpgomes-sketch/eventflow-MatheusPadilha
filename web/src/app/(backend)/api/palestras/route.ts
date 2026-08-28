a03bb8645d3cf8eeadabc91b9d8fa146c5a1769f
matheusrpgomes-sketch <matheusrpgomes@gmail.com>
2026-08-28 14:37:23 -0300

    feat(API-Palestra): First API function [GET]

---
+++ web/src/app/(backend)/api/palestras/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { listarPublicadas } from "../../services/palestras";
+import { NumeroDePalestra } from "../../services/palestras";
+import { zodErrorHandler } from "@/utils/api/server";
+
+export async function GET(request: NextRequest) {
+   try {
+        const palestras = await listarPublicadas();
+        return NextResponse.json(palestras);
+    }catch (error) {
+        return zodErrorHandler(error);
+
+    }
+}
